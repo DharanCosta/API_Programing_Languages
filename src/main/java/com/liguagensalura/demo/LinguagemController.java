738ae4006dc82905e24031b0e7bb7b8f8e31f657
Dharan Costa <dharancosta@gmail.com>
2022-07-22 13:06:40 -0300

    Deploy inicial

--- src/main/java/com/liguagensalura/demo/LinguagemController.java
+++ src/main/java/com/liguagensalura/demo/LinguagemController.java
@@ -14,29 +14,7 @@ public class LinguagemController {
     @Autowired
     private LinguagemRepository repository;
 
-    private DtoService dtoService;
-
-    @GetMapping("/linguagens-api")
-    public List<String> obterApi(){
-        List<Linguagem> linguagemList = repository.findAll();
-        List<LinguagemDTO> novaList = new ArrayList<>();
-
-        List<String> collect = linguagemList.stream()
-                .map(object -> Objects.toString(object, null))
-                .collect(Collectors.toList());
-//
-//        for (Linguagem item : linguagemList) {
-//            int i =0;
-//            novaList.(item.getTitle());
-//            novaList.get(i).setImagem(item.getImage());
-//            String s =String.valueOf(item.getRating());
-//            novaList.get(i).setRanking(s);
-//            i++;
-//        }
-        return collect;
-    }
-
-    @GetMapping("/linguagens")
+     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens(){
         List<Linguagem> linguagemList = repository.findAll();
         return linguagemList;
@@ -49,26 +27,20 @@ public class LinguagemController {
                 .collect(Collectors.toList());
         return listRanked;
     }
-
     @PostMapping("/linguagens")
     public ResponseEntity<Linguagem> cadastroLinguagem(@RequestBody Linguagem linguagem){
         Linguagem linguagemSalva = repository.save(linguagem);
         return ResponseEntity.ok(linguagemSalva);
     }
-
     @PutMapping("/linguagens/{id}")
     public ResponseEntity<Linguagem> atualizarLinguagem(@PathVariable String id, @RequestBody Linguagem linguagem) {
         Linguagem novaLinguagem = repository.findById(id).orElseThrow();
-
         novaLinguagem.setTitle(linguagem.getTitle());
         novaLinguagem.setImage(linguagem.getImage());
         novaLinguagem.setRating(linguagem.getRating());
-
         repository.save(novaLinguagem);
-
         return ResponseEntity.ok(novaLinguagem);
     }
-
     @DeleteMapping("linguagens/{id}")
     public String deletarLinguagem(@PathVariable String id){
         try{
